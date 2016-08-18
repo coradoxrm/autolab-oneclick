59bd44b20bbb112ddf44c5ad5fd694997f4ecad9
coradoxrm <coradoxrm@163.com>
2016-08-18 11:07:01 -0400

    build from image

--- install.sh
+++ install.sh
@@ -2,17 +2,6 @@ echo "downloading source file..."
 git clone https://github.com/autolab/Tango.git
 git clone https://github.com/autolab/Autolab
 
-echo "copying config files into project files..."
-cp configs/Tango/start.sh Tango/start.sh
-cp configs/Tango/config.py Tango/config.py
-cp configs/Autolab/autogradeConfig.rb Autolab/config/autogradeConfig.rb
-cp configs/Autolab/devise.rb Autolab/config/initializers/devise.rb
-cp configs/Autolab/nginx.conf Autolab/docker/nginx.conf
-cp configs/Autolab/production.rb Autolab/config/environments/production.rb
-
-cp configs/Autolab/Dockerfile Autolab/Dockerfile
-cp configs/Autolab/seeds.rb Autolab/db/seeds.rb
-
 echo "make volumes..."
 mkdir Autolab/courses
 sudo chown -R 9999:9999 Autolab/courses
