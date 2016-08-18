d63a5a4034273a4de3f514ce6b856798a26da86a
coradoxrm <coradoxrm@163.com>
2016-08-18 12:49:50 -0400

    fix compose download

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo "[1/5]Installing docker and docker-compose"
 sudo apt-get update && sudo apt-get install -y vim git curl
 curl -sSL https://get.docker.com/ | sh
 
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose -`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
