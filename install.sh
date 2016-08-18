c9937174c71346a587ee9153b6c52c0bdf510137
coradoxrm <coradoxrm@163.com>
2016-08-18 12:11:25 -0400

    sleep to wait mysql done

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
-echo "downloading source file..."
+echo "[1/4] Downloading source file..."
 git clone https://github.com/autolab/Tango.git
 git clone https://github.com/autolab/Autolab
 
-echo "make volumes..."
+echo "[2/4] Mounting volumes..."
 mkdir Autolab/courses
 sudo chown -R 9999:9999 Autolab/courses
 mkdir db-data
 
-echo "init docker images and containers..."
+echo "[3/4] Init docker images and containers..."
 docker-compose up -d
 
-wait
+sleep 10
 
-echo "init database..."
+echo "[4/4] Init database..."
 docker-compose run --rm -e RAILS_ENV=production web rake db:create
 docker-compose run --rm -e RAILS_ENV=production web rake db:migrate
 docker-compose run --rm -e RAILS_ENV=production web rake db:seed
 
-echo "now you can use an admin account\n Username: admin@foo.bar\n password : 11111111\n to try Autolab for trail"
+echo "[Autolab installation finished]\n Now you can use an admin account\n Username: admin@foo.bar\n password : 11111111\n to try Autolab for trail"
 
 
