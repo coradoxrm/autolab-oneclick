86e14fa375d62a67104d95aaaa60d90316a94366
coradoxrm <coradoxrm@163.com>
2016-08-24 16:00:26 -0400

    a stable version

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ cp configs/Autolab/autogradeConfig.rb Autolab/config/autogradeConfig.rb
 cp configs/Autolab/devise.rb Autolab/config/initializers/devise.rb
 cp configs/Autolab/nginx.conf Autolab/docker/nginx.conf
 cp configs/Autolab/production.rb Autolab/config/environments/production.rb
-
+cp configs/Autolab/Dockerfile Autolab/Dockerfile
 cp configs/Autolab/seeds.rb Autolab/db/seeds.rb
 
 echo "make volumes..."
 mkdir Autolab/courses
-mkdir db-data
+sudo chown -R 9999:9999 Autolab/courses
 
 echo "init docker images and containers..."
 docker-compose up -d
