08099301bace394bb2595a17d78c70abf292b3d9
renmiao <coradoxrm@163.com>
2016-08-31 15:15:42 -0400

    local build from image and add ssl folder to server

--- uninstall.sh
+++ uninstall.sh
@@ -7,8 +7,8 @@ sudo rm -rf ./local/Autolab ./local/Autolab ./local/Tango ./local/db-data
 
 sudo rm -rf /var/lib/docker
 
-sudo apt-get purge docker-engine
-sudo apt-get autoremove --purge docker-engine
-sudo apt-get autoclean
+sudo apt-get purge docker-engine;
+sudo apt-get autoremove --purge docker-engine;
+sudo apt-get autoclean;
 
 echo "Now you can delete the whole directory of Autolab-project"
